c2ceca58a980f4656a2e80b62d52372aea20e0ab
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-30 17:31:14 +0600

    Modified next-auth secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -70,7 +70,7 @@ const handler = NextAuth({
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET as string,
   pages: {
     signIn: "/signin",
   },
